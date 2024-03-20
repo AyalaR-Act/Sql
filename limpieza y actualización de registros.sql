0a07e4e2e1f5df3afd441b97ce15cf2127a47ee4
Isela Ayala Ramírez <iayalar001@alumno.uaemex.mx>
2024-03-20 15:34:11 -0600

    Update limpieza y actualización de registros.sql

--- limpieza y actualización de registros.sql
+++ limpieza y actualización de registros.sql
@@ -1,4 +1,9 @@
+-- Limpieza de datos con lenguaje sql
+-- Anotaciones: Cargar el archivo bd1 y crear una base de datos 
+-- La base de datos sobre la que se trabajó en clean 
+CREATE DATABASE clean;
 use clean;
+-- Creamos un proceso almacenado para visualiazar la tabla de manera mas eficiente
 DELIMITER //
 CREATE PROCEDURE alldata()
 BEGIN
@@ -6,7 +11,9 @@ select * from dataorganization;
 END //
 DELIMITER ;
 call alldata(); 
+
 set sql_safe_updates = 0;
+
 -- Renombramos las columnas de la tabla
 alter table dataorganization change column `﻿COD` id_cliente int not null;
 ALTER TABLE dataorganization
@@ -22,6 +29,7 @@ alter table dataorganization change column `País de origen` country varchar(50)
 alter table dataorganization change column `Tipo de contrato` contract varchar(50) null;
 
 -- Revisemos si existen espacios en los registros
+-- Realizamos las modificaciones correspondientes para eliminar los espacios
 call alldata();
 Select nombres from dataorganization 
 where nombres regexp '\\s{2,}';
@@ -41,7 +49,7 @@ when start_date like '%-%' then  date_format(str_to_date(start_date, '%m-%d-%Y')
 else null
 end as new_date
 from dataorganization;
--- aplicamos la actualización
+-- Aplicamos la actualización
 UPDATE dataorganization
 SET start_date = CASE 
                      WHEN start_date LIKE '%/%' THEN DATE_FORMAT(STR_TO_DATE(start_date, '%m/%d/%Y'), '%Y-%m-%d')
@@ -49,7 +57,7 @@ SET start_date = CASE
                      ELSE NULL
                  END;
                  alter table dataorganization modify column start_date date;
--- observemos ahora los cambios realizados
+-- Observemos ahora los cambios realizados
 call alldata();
 describe dataorganization;                 
 -- Eliminemos ahora los duplicados de la tabla 
@@ -75,28 +83,32 @@ select distinct * from duplicados;
 -- Contabilicemos los datos que contiene cada tabla para verificar la eliminación de duplicados
 select count(*) as original from duplicados;
 select count(*) as original from temp_data;
--- Notemos que sólo se eliminaron 17 registros de 20 verifiquemos si existen duplicados nuevamente
+-- Notemos que no se eliminaron todos los registros, verifiquemos si existen duplicados nuevamente
 create table dataorganization as select * from temp_data;
--- realizamos consulta de duplicados
+-- Realizamos consulta de duplicados
 select id_cliente, count(*) as cantidad_duplicados
 from dataorganization
 group by id_cliente
 having count(*) > 1;
--- Notemos que los duplicados son los que tienen el identificador 
--- 4412, 2189, 2376, 7540
--- un total de 4 duplicados evaluemos la causa raíz realizamos la consulta
+-- Notemos que los duplicados son los que tienen el identificador (Id_cliente) 
+-- Es importante solucionar este error puesto que para ser llave primaria deberá cumplir el que id_cliente sea unico para cada registro
+-- Los id_cliente duplicados son :  4412, 2189, 2376, 7540
+-- Tenemos un total de 4 duplicados evaluemos la causa raíz realizamos la consulta
 SELECT *
 FROM dataorganization
-WHERE id_cliente IN (9977);
+WHERE id_cliente IN (4412, 2189, 2376, 7540);
 -- Observamos que el problema es que existe una misma asignación de Id para distintas personas, esto causará problemas
 -- dado que el id_cliente es el identificador unico (llave primaria)
 -- Generamos  un nuevo número de id_cliente que no esté en uso
 SET @nuevo_id := (SELECT MAX(id_cliente) + 1 FROM dataorganization);
--- Actualizamos el registro duplicado con el nuevo número de id_cliente 
+-- Actualizamos el registro duplicado con el nuevo número de id_cliente
+-- Ahora modificamos el id_cliente para los registros identificados 
+-- Se hará un cambio en el id_cliente a ciertos registros y lo haremos por la columna nombres
 call alldata();
 UPDATE dataorganization
 SET id_cliente = @nuevo_id
 WHERE nombres = 'grantley boatwright';
+
 update dataorganization set id_cliente =@nuevo_id 
 where nombres ='cindelyn weight';
 
@@ -122,14 +134,18 @@ having count(*) > 1
 ) as subquery;
 -- Al realizar la consulta notamos que ya no existen duplicados en la tabla 
 call alldata();
--- Asignar nulos en cuadros vacios
+
+-- Modifiquemos ahora la columna salary
+-- Asignemos datos nulos en cuadros vacios
 UPDATE dataorganization 
 SET salary = NULL
 WHERE salary IS NULL OR salary = '' OR TRIM(salary) = '';
+-- Eliminamos el simbolo de pesos u otro factor que no permita tener el registro como tipo de dato int o double
 select salary,  cast(trim(replace(replace (salary, '$', ''), ',','')) as decimal (15, 2)) as salary1 from dataorganization;
 UPDATE dataorganization 
 SET salary = CAST(TRIM(REPLACE(REPLACE(salary, '$', ''), ',', '')) AS DECIMAL(15, 2))
 WHERE salary IS NOT NULL;
+-- Asignamos un tipo de dato int a la  columna salary
 alter table dataorganization modify column salary int;
 -- Elaboremos de manera masiva cuentas de correo electrónico
 -- realizamos una visualización 
